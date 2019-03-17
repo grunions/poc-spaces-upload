18e7850e1a858736d88efbaa2c7622b87f3b63b2
Paul <paul@zom.bi>
2019-03-17 17:01:17 +0100

    Concept: Local Blob you can write to

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"archive/tar"
+	"archive/zip"
 	"compress/gzip"
 	"crypto/sha256"
 	"fmt"
@@ -25,6 +26,17 @@ import (
 	"github.com/pkg/errors"
 )
 
+type Package interface {
+	IsDir()
+	Reference()
+	Size()
+	OriginalSize()
+}
+
+//type Blob interface {
+//	io.ReadWriteCloser
+//}
+
 type S3Config struct {
 	Key      string
 	Secret   string
@@ -41,6 +53,21 @@ type Config struct {
 var config *Config
 var tpl *template.Template
 
+type BlobStore struct {
+	config S3Config
+	client *minio.Client
+}
+
+//func NewBlobStore(config S3Config) *BlobStore {
+//
+//}
+
+// Future methods?
+// Put(blob) error {}
+// Stat(blob) (ObjectInfo, error) {}
+// Get(blob) (io.ReadCloser, error) {}
+// Delete(blob) error {}
+
 func init() {
 	config = &Config{}
 	config.S3 = S3Config{
@@ -56,12 +83,18 @@ func init() {
 
 }
 
-// Blob is a gzip compressed object, which may either be a single file
+// LocalBlob is a gzip compressed object, which may either be a single file
 // or a directory in a tar file
-type Blob struct {
-	IsDir bool
+type LocalBlob struct {
+	IsDir     bool
+	Reference string
+	// Size()
+	// UncompressedSize()
+	// Hash()
 
 	File *os.File
+
+	pw *pb.ProgressBar
 
 	gw  io.WriteCloser             // gzip writer for compression
 	hw  hash.Hash                  // hashwriter for checksum
@@ -72,11 +105,12 @@ type Blob struct {
 	// A human readable reference, for example a filename associated with the
 	// blob, e.g. "Human Music.mp3". This is non-unique, user-controlled and
 	// must not be used for any logic.
-	Reference string
 }
 
-func NewBlob() (*Blob, error) {
-	blob := &Blob{
+// NewLocalBlob creates a new blob with a temporary file, which MUST be
+// deleted after all related actions are complete.
+func NewLocalBlob() (*LocalBlob, error) {
+	blob := &LocalBlob{
 		IsDir: false,
 	}
 
@@ -87,39 +121,49 @@ func NewBlob() (*Blob, error) {
 		return nil, errors.Wrap(err, "Blob: could not create temporary file")
 	}
 
+	// progress bar
+	blob.pw = pb.New(0)
+	blob.pw.SetUnits(pb.U_BYTES)
+	blob.pw.ShowSpeed = true
+	blob.pw.ShowPercent = false
+	blob.pw.ShowTimeLeft = false
+	blob.pw.ShowBar = false
+	blob.pw.Start()
+
 	blob.ccw = datacounter.NewWriterCounter(blob.File)
 	blob.gw, _ = gzip.NewWriterLevel(blob.ccw, gzip.BestCompression)
 	blob.ucw = datacounter.NewWriterCounter(blob.gw)
 	blob.hw = sha256.New()
-	blob.mw = io.MultiWriter(blob.ucw, blob.hw)
+	blob.mw = io.MultiWriter(blob.ucw, blob.hw, blob.pw)
 
 	return blob, nil
 }
 
 // Close finishes the writing process to the blob
-func (blob *Blob) Close() error {
+func (blob *LocalBlob) Close() error {
+	blob.pw.Finish()
 	blob.gw.Close()
 	return blob.File.Close()
 }
 
 // Size returns the Compressed blob size
-func (blob *Blob) Size() int64 {
+func (blob *LocalBlob) Size() int64 {
 	return int64(blob.ccw.Count())
 }
 
 // UncompressedSize returns the original size, or the size of the
 // Tar file if the blob is a dir blob
-func (blob *Blob) UncompressedSize() int64 {
+func (blob *LocalBlob) UncompressedSize() int64 {
 	return int64(blob.ucw.Count())
 }
 
 // Hash returns the checksum of the uncompressed data
-func (blob *Blob) Hash() []byte {
+func (blob *LocalBlob) Hash() []byte {
 	return blob.hw.Sum(nil)
 }
 
 // Write implements the standard Write interface
-func (blob *Blob) Write(b []byte) (n int, err error) {
+func (blob *LocalBlob) Write(b []byte) (n int, err error) {
 	return blob.mw.Write(b)
 }
 
@@ -128,16 +172,16 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func Reader(client *minio.Client, hash []byte) (io.ReadCloser, error) {
-	o, err := client.GetObject(config.S3.Bucket, fmt.Sprintf("blob/%x", hash), minio.GetObjectOptions{})
+	o, err := client.GetObject(config.S3.Bucket, fmt.Sprintf("blob/%x.gz", hash), minio.GetObjectOptions{})
 	if err != nil {
 		return nil, err
 	}
 	return o, nil
 }
 
-func ReaderToBlob(fr io.Reader) (blob *Blob, e error) {
+func ReaderToBlob(fr io.Reader) (blob *LocalBlob, e error) {
 
-	blob, err := NewBlob()
+	blob, err := NewLocalBlob()
 	if err != nil {
 		blob.Close()
 		os.Remove(blob.File.Name()) // try to clean up
@@ -158,8 +202,8 @@ func ReaderToBlob(fr io.Reader) (blob *Blob, e error) {
 }
 
 // CheckDuplicate return true if a duplicate exists
-func CheckDuplicate(client *minio.Client, blob *Blob) bool {
-	remoteFilename := fmt.Sprintf("blob/%x", blob.Hash)
+func CheckDuplicate(client *minio.Client, blob *LocalBlob) bool {
+	remoteFilename := fmt.Sprintf("blob/%x.gz", blob.Hash())
 	o, err := client.GetObject(config.S3.Bucket, remoteFilename, minio.GetObjectOptions{})
 	if err != nil {
 		return false
@@ -180,8 +224,8 @@ func CheckDuplicate(client *minio.Client, blob *Blob) bool {
 	return true
 }
 
-func UploadBlob(client *minio.Client, blob *Blob) error {
-	remoteFilename := fmt.Sprintf("blob/%x", blob.Hash())
+func UploadBlob(client *minio.Client, blob *LocalBlob) error {
+	remoteFilename := fmt.Sprintf("blob/%x.gz", blob.Hash())
 
 	bar := pb.New64(blob.Size())
 	bar.ShowSpeed = true
@@ -252,7 +296,7 @@ func TarDir(src string, writer io.Writer) error {
 			return err
 		}
 
-		// return on non-regular files for this suggested update)
+		// return on non-regular files)
 		if !fi.Mode().IsRegular() {
 			return nil
 		}
@@ -355,6 +399,119 @@ func Untargz(dst string, r io.Reader) error {
 	}
 }
 
+func TarZip(reader io.ReaderAt, size int64, writer io.Writer) error {
+	zr, err := zip.NewReader(reader, size)
+	if err != nil {
+		return errors.Wrap(err, "Could not open zip reader")
+	}
+
+	tw := tar.NewWriter(writer)
+	defer tw.Close()
+
+	copyBuffer := make([]byte, 32*1024)
+
+	for _, f := range zr.File {
+		fi := f.FileInfo()
+
+		// create a new dir/file header
+		header, err := tar.FileInfoHeader(fi, fi.Name())
+		if err != nil {
+			return err
+		}
+
+		// reset modification time, to make output deterministic
+		header.ModTime = time.Time{}
+
+		// update the name to correctly reflect the desired destination when untaring
+		header.Name = strings.TrimPrefix(fi.Name(), string(filepath.Separator))
+
+		// write the header
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		// return on non-regular files)
+		if !fi.Mode().IsRegular() {
+			return nil
+		}
+
+		// open files for taring
+		fr, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		// copy file data into tar writer
+		if _, err := io.CopyBuffer(tw, fr, copyBuffer); err != nil {
+			return err
+		}
+
+		// manually close here after each file operation; defering would cause each file close
+		// to wait until all operations have completed.
+		fr.Close()
+	}
+
+	return tw.Close()
+}
+
+func UploadDir(client *minio.Client, src string) ([]byte, error) {
+	blob, err := NewLocalBlob()
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to prepare dir blob")
+	}
+	blob.IsDir = true
+	defer os.Remove(blob.File.Name())
+	defer blob.Close()
+
+	if err := TarDir(src, blob); err != nil {
+		return nil, errors.Wrap(err, "Failed to tar dir")
+	}
+	if err := blob.Close(); err != nil {
+		return nil, errors.Wrap(err, "Failed to flush blob dir")
+	}
+
+	if CheckDuplicate(client, blob) {
+		// already exists, exit early
+		return blob.Hash(), nil
+	}
+
+	if err := UploadBlob(client, blob); err != nil {
+		return blob.Hash(), errors.Wrap(err, "Failed to upload dir")
+	}
+
+	return blob.Hash(), nil
+}
+
+// example for uploading a local directory
+func xupl(w http.ResponseWriter, r *http.Request) {
+	client, err := minio.New(
+		config.S3.Endpoint,
+		config.S3.Key,
+		config.S3.Secret,
+		config.S3.SSL)
+	if err != nil {
+		log.Printf("Error: %s", err)
+		fmt.Fprintf(w, "Error\n")
+		return
+	}
+
+	src := "/tmp/to-upload"
+
+	fmt.Fprintln(w, "Processing upload..")
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
+
+	checksum, err := UploadDir(client, src)
+	if err != nil {
+		fmt.Fprintln(w, "Failed uploading dir")
+		log.Printf("Error: %s", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Uploaded dir with checksum %x\n", checksum)
+}
+
 func directoryUpload(w http.ResponseWriter, req *http.Request) {
 	const name = "files"
 
@@ -386,7 +543,7 @@ func directoryUpload(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	blob, err := NewBlob()
+	blob, err := NewLocalBlob()
 	if err != nil {
 		fmt.Fprintln(w, "error opening archive")
 		log.Printf("error creating blob: %s", err)
@@ -398,6 +555,9 @@ func directoryUpload(w http.ResponseWriter, req *http.Request) {
 
 	tw := tar.NewWriter(blob)
 	defer tw.Close()
+
+	// reusable buffer for io.CopyBuffer
+	copyBuffer := make([]byte, 32*1024)
 
 	for _, file := range files {
 		fr, err := file.Open()
@@ -416,14 +576,13 @@ func directoryUpload(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 
-		_, err = io.Copy(tw, fr)
+		_, err = io.CopyBuffer(tw, fr, copyBuffer)
 		if err != nil {
 			fmt.Fprintln(w, err)
 			log.Print(err)
 			return
 		}
 
-		fmt.Fprintf(w, "Uploaded %s\n", file.Filename)
 		fr.Close()
 	}
 
@@ -456,7 +615,8 @@ func multiUpload(w http.ResponseWriter, req *http.Request) {
 
 	// parse forms, storing everything thats bigger than
 	// 30mb on disk.
-	if err := req.ParseMultipartForm(30 << 20); err != nil {
+	const size30mb = 30 << 20
+	if err := req.ParseMultipartForm(size30mb); err != nil {
 		fmt.Fprintln(w, err)
 		return
 	}
@@ -490,7 +650,7 @@ func multiUpload(w http.ResponseWriter, req *http.Request) {
 
 		if CheckDuplicate(client, blob) {
 			// file was already uploaded
-			fmt.Fprintf(w, "replaced %10d byte: %s\n", blob.UncompressedSize, file.Filename)
+			fmt.Fprintf(w, "replaced %10d byte: %s\n", blob.UncompressedSize(), file.Filename)
 			continue
 		}
 
@@ -501,7 +661,7 @@ func multiUpload(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 
-		fmt.Fprintf(w, "Uploaded %10d byte: %s\n", blob.UncompressedSize, file.Filename)
+		fmt.Fprintf(w, "Uploaded %10d byte: %s\n", blob.UncompressedSize(), file.Filename)
 	}
 	fmt.Fprintln(w, "Upload completed")
 	return
@@ -510,6 +670,7 @@ func multiUpload(w http.ResponseWriter, req *http.Request) {
 func main() {
 	router := chi.NewMux()
 	router.Get("/", index)
+	router.Get("/x", xupl)
 	router.Post("/upload-dir", directoryUpload)
 	router.Post("/upload-files", multiUpload)
 	http.ListenAndServe(":8000", router)
